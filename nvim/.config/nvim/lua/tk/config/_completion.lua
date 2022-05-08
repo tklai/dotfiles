c257783e9bfd40b0e298d79cfb1e534dbbe00b8c
TK Lai <iam@tklai.net>
2022-05-08 17:20:16 +0800

    feat(neovim): update cmp config

--- nvim/.config/nvim/lua/tk/config/_completion.lua
+++ nvim/.config/nvim/lua/tk/config/_completion.lua
@@ -1,5 +1,32 @@
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.opt.shortmess:append("c")
+
+local icons = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "⌘",
+  Field = "ﰠ",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "祐",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "פּ",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
 
 local cmp = require("cmp")
 
@@ -24,9 +51,38 @@ cmp.setup({
     { name = "path" },
     { name = "buffer" },
   }),
+  completion = {
+    completeopt = "menu,menuone,noselect",
+    keyword_length = 1,
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(_, vim_item)
+      vim_item.menu = vim_item.kind
+      vim_item.kind = icons[vim_item.kind]
+
+      return vim_item
+    end,
+  },
   window = {
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
     },
   },
+})
+
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
