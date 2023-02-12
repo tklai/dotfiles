4ac90a3bfe5028c41ff59464442ed215130608c8
TK Lai <iam@tklai.net>
2023-02-12 21:35:12 +0800

    fix(neovim): will this improve the completion?

--- nvim/.config/nvim/after/plugin/completion.lua
+++ nvim/.config/nvim/after/plugin/completion.lua
@@ -28,7 +28,15 @@ local icons = {
   TypeParameter = "",
 }
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
   snippet = {
@@ -43,13 +51,36 @@ cmp.setup({
     -- DO NOT SET `select` to true in `confirm()`.
     -- Otherwise, the first item will be selected even you didn't select anything.
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    -- Tab
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+        -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   }),
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
+    { name = "nvim_lsp", keyword_length = 1 },
+    { name = "luasnip", keyword_length = 2 },
   }, {
-    { name = "buffer" },
+    { name = "path" },
+    { name = "buffer", keyword_length = 3 },
   }),
   completion = {
     completeopt = "menu,menuone,noselect",
