5126a8845a25afdfd8d7641e6fac9aee97271c74
TK Lai <iam@tklai.net>
2023-03-12 20:58:51 +0800

    neovim: cmp: somehow fixed cmdline completion

--- nvim/.config/nvim/lua/custom/config/completion.lua
+++ nvim/.config/nvim/lua/custom/config/completion.lua
@@ -129,19 +129,3 @@ cmp.setup({
     ghost_text = false,
   },
 })
-
-cmp.setup.cmdline("/", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = "buffer" },
-  },
-})
-
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
-})
