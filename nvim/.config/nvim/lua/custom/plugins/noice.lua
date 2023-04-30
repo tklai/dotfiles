929eb3d7d437a221f2827884abd3cac99adeb557
TK Lai <iam@tklai.net>
2023-04-30 17:31:31 +0800

    neovim: nvim-notify: make the notification show bottom up

--- nvim/.config/nvim/lua/custom/plugins/noice.lua
+++ nvim/.config/nvim/lua/custom/plugins/noice.lua
@@ -18,23 +18,6 @@ return {
         long_message_to_split = true,
         lsp_doc_border = true,
       },
-      views = {
-        cmdline_popup = {
-          border = {
-            style = "single",
-          },
-        },
-        popupmenu = {
-          border = {
-            style = "none",
-          },
-        },
-        popup = {
-          border = {
-            style = "none",
-          },
-        },
-      },
     },
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -42,7 +25,52 @@ return {
         "rcarriga/nvim-notify",
         opts = {
           background_colour = "#E1E2E6",
-          stages = "fade",
+          -- stages = "fade",
+          stages = {
+            function(state)
+              local next_height = state.message.height + 2
+              local next_row =
+                require("notify.stages.util").available_slot(state.open_windows, next_height, "bottom_up")
+              if not next_row then
+                return nil
+              end
+              return {
+                relative = "editor",
+                anchor = "SE",
+                width = state.message.width,
+                height = state.message.height,
+                col = vim.opt.columns:get(),
+                row = next_row,
+                border = "rounded",
+                style = "minimal",
+                opacity = 0,
+              }
+            end,
+            function()
+              return {
+                opacity = { 100 },
+                col = { vim.opt.columns:get() },
+              }
+            end,
+            function()
+              return {
+                col = { vim.opt.columns:get() },
+                time = true,
+              }
+            end,
+            function()
+              return {
+                opacity = {
+                  0,
+                  frequency = 2,
+                  complete = function(cur_opacity)
+                    return cur_opacity <= 4
+                  end,
+                },
+                col = { vim.opt.columns:get() },
+              }
+            end,
+          },
         },
         keys = {
           {
