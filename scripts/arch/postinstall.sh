04970598d24752c3f987f1faebbb83a46dc264c4
TK Lai <iam@tklai.net>
2023-07-29 22:20:16 +0800

    archlinux: add postinstall script

---
+++ scripts/arch/postinstall.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Check it is ArchLinux and exit the script if it is not
+if [ ! -f /etc/arch-release ]; then
+    echo "This script is only for ArchLinux."
+    exit 1
+fi
+
+# Prompt the user to enter the password for further sudo use
+echo "Please enter your password for sudo use."
+sudo -v
+
+# Update package list
+echo "Updating package list..."
+sudo pacman -Sy
+
+# Install essential packages
+echo "Installing essential packages..."
+sudo pacman -S \
+    zsh \
+    htop \
+    neofetch \
+    wget \
+    curl \
+    git \
+    vi \
+    vim \
+    neovim \
+    python \
+    ripgrep \
+    fzf \
+    unzip \
+    p7zip \
+    ufw \
+    stow \
+    exa \
+    starship \
+    ;
+
+# Install fonts
+echo "Installing fonts..."
+sudo pacman -S \
+    noto-fonts \
+    noto-fonts-cjk \
+    noto-fonts-emoji \
+    ttf-jetbrains-mono-nerd \
+    ;
+
+# Install GUI packages
+echo "Installing GUI packages..."
+sudo pacman -S \
+    flatpak \
+    # Terminals
+    # alacritty \
+    kitty \
+    # wezterm \
+    # Browsers
+    firefox \
+    # IME
+    fcitx5-im \
+    fcitx5-rime \
+    librime \
+    rime-cangjie \
+    rime-quick \
+    ;
+
+# Check r8168 is required for the network card
+if [ lspci | grep -i ethernet | grep -i realtek | grep -i 8168 ]; then
+    echo "Installing r8168 for the network card..."
+    sudo pacman -S r8168
+    sudo echo "blacklist r8169" > /etc/modprobe.d/blacklist_r8169.conf
+    sudo mkinitcpio -p linux
+fi
+
+# Check CPU vendor and install corresponding microcode
+if [ grep -i intel /proc/cpuinfo ]; then
+    echo "Installing intel-ucode..."
+    sudo pacman -S intel-ucode
+elif [ grep -i amd /proc/cpuinfo ]; then
+    echo "Installing amd-ucode..."
+    sudo pacman -S amd-ucode
+fi
+
+# Install yay
+echo "Installing yay..."
+sudo pacman -S --needed base-devel
+mkdir -p ~/Tools
+cd ~/Tools
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "All done!"
