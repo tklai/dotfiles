3170a00c6dd3e43f204969184625155ea53e50b1
TK Lai <iam@tklai.net>
2023-10-15 15:54:47 +0800

    wezterm: store configs in a variable and extract theme part

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
-local status_ok, wezterm = pcall(require, "wezterm")
-if not status_ok then
-  return {}
+local ok, wezterm = pcall(require, "wezterm")
+if not ok then
+  print("This file can only be run by WezTerm.")
+
+  os.exit()
 end
 
-return {
+local configs = {
   audible_bell = "Disabled",
   font = wezterm.font_with_fallback({
     {
@@ -32,29 +34,33 @@ return {
   },
   -- hide_tab_bar_if_only_one_tab = true,
 
-  -- Kanagawa
-  -- force_reverse_video_cursor = true,
-  -- colors = {
-  --   foreground = "#dcd7ba",
-  --   background = "#1f1f28",
-  --
-  --   cursor_bg = "#c8c093",
-  --   cursor_fg = "#c8c093",
-  --   cursor_border = "#c8c093",
-  --
-  --   selection_fg = "#c8c093",
-  --   selection_bg = "#2d4f67",
-  --
-  --   scrollbar_thumb = "#16161d",
-  --   split = "#16161d",
-  --
-  --   ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-  --   brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-  --   indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-  -- },
-  color_scheme = "Catppuccin Latte",
-  colors = {
-    -- background = "#E1E2E6",
-  },
   window_background_opacity = 0.95,
 }
+
+-- Themes
+-- Kanagawa
+-- configs.force_reverse_video_cursor = true
+-- configs.colors = {
+--   foreground = "#dcd7ba",
+--   background = "#1f1f28",
+--
+--   cursor_bg = "#c8c093",
+--   cursor_fg = "#c8c093",
+--   cursor_border = "#c8c093",
+--
+--   selection_fg = "#c8c093",
+--   selection_bg = "#2d4f67",
+--
+--   scrollbar_thumb = "#16161d",
+--   split = "#16161d",
+--
+--   ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+--   brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+--   indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+-- }
+
+
+-- Catppuccin
+configs.color_scheme = "Catppuccin Latte"
+
+return configs
