44e0613ab0897e7fd02feeac16a784ceb9bd704b
TK Lai <iam@tklai.net>
2024-08-18 21:09:53 +0800

    fix(zsh): fix fnm loading in mac

--- zsh/.zshrc.d/99-fnm.zsh
+++ zsh/.zshrc.d/99-fnm.zsh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
 
+# If fnm is not installed as system pacakge, append the path to execution path.
 FNM_PATH="$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
   export PATH="$FNM_PATH:$PATH"
-  eval "$(fnm env --use-on-cd)"
+fi
+
+# This should look for the executable from all paths, including newly appended
+# path and system installed path.
+if [ -x "$(command -v fnm)" ]; then
+  eval "$(fnm env --use-on-cd --shell zsh)"
 fi
