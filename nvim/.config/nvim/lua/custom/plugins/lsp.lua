394f648a530abab0238ce2176cca5d596f760d09
TK Lai <iam@tklai.net>
2025-03-09 12:23:07 +0800

    neovim: namu

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -274,4 +274,29 @@ return {
     config = true,
     cmd = "Outline",
   },
+  {
+    "bassamsdata/namu.nvim",
+    lazy = true,
+    cmd = "Namu",
+    opts = {
+        -- Enable the modules you want
+        namu_symbols = {
+          enable = true,
+          options = {}, -- here you can configure namu
+        },
+        -- Optional: Enable other modules if needed
+        ui_select = { enable = false }, -- vim.ui.select() wrapper
+        colorscheme = {
+          enable = false,
+          options = {
+            -- NOTE: if you activate persist, then please remove any vim.cmd("colorscheme ...") in your config, no needed anymore
+            persist = true, -- very efficient mechanism to Remember selected colorscheme
+            write_shada = false, -- If you open multiple nvim instances, then probably you need to enable this
+          },
+        },
+    },
+    keys = {
+      { "<leader>ss", ":Namu symbols<cr>", { desc = "Jump to LSP symbol", silent = true }},
+    },
+  }
 }
