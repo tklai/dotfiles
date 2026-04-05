748390d9b12154e203b763e9f0f05de50c3e9bb1
TK Lai <iam@tklai.net>
2026-04-05 14:41:46 +0800

    script: put turbo boost script to the end

--- scripts/disable-intel-turbo-boost.sh
+++ scripts/disable-intel-turbo-boost.sh
@@ -5,7 +5,7 @@ if [ -f "/sys/devices/system/cpu/intel_pstate/no_turbo" ]; then
     
     if [ -x "$(command -v systemctl)" ]; then
         echo "Adding conf to disable Intel Turbo Boost on boot with systemd."
-        echo 'w /sys/devices/system/cpu/intel_pstate/no_turbo - - - - 1' | sudo tee /etc/tmpfiles.d/disable-intel-turbo-boost.conf
+        echo 'w /sys/devices/system/cpu/intel_pstate/no_turbo - - - - 1' | sudo tee /etc/tmpfiles.d/99-disable-intel-turbo-boost.conf
     fi
 else
     echo "Team AMD!"
